0dde33c23eb66606fccd89760d2a9a94407bbd3c
Nikki <piknik@me.com>
2020-12-04 06:21:40 -0700

    finish library and library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,38 @@
+function createLibrary(library) {
+  return {
+    name: library,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+    return library.shelves.fantasy.push(book)
+  } else if (book.genre === 'nonFiction') {
+    return library.shelves.nonFiction.push(book)
+  } else if (book.genre === 'fiction') {
+    return library.shelves.fiction.push(book)
+  }
+}
+
+function checkoutBook(library, title, genre) {
+  if (library.shelves[genre].length === 0)  {
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  } else if (library.shelves[genre][0].title === title) {
+    library.shelves[genre] = []
+    return `You have now checked out ${title} from the ${library.name}`
+  }
+
+
+
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
